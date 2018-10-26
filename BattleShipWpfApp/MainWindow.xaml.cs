e15da13ac6de691e7952efdc7489132e3b1aa6b5
jaco2716 <jacob.welin@hotmail.com>
2018-10-26 13:35:40 +0200

    Initial commit

--- BattleShipWpfApp/MainWindow.xaml.cs
+++ BattleShipWpfApp/MainWindow.xaml.cs
@@ -24,11 +24,26 @@ namespace BattleShipWpfApp
         int gridSize = 10;
         String[,] gridArray;
         Button[,] buttonArray;
+        Random random = new Random();
+        public int randomNumberX;
+        public int randomNumberY;
+        List<int> dimensionX = new List<int>();
+        List<int> dimensionY = new List<int>();
+        private int shotsFired = 0;
+        private int ships = 14;
 
         public MainWindow()
         {
 
             InitializeComponent();
+            CreateButtons();
+            CreateBattleShip();
+            //Reveal();
+
+        }
+
+        private void CreateButtons()
+        {
             gridArray = new String[gridSize, gridSize];
             buttonArray = new Button[gridSize, gridSize];
 
@@ -43,28 +58,237 @@ namespace BattleShipWpfApp
                 ViewGrid.ColumnDefinitions.Add(cd);
             }
 
+            for (int X = 0; X < gridSize; X++)
+            {
+                for (int Y = 0; Y < gridSize; Y++)
+                {
+                    Button btn = new Button();
+                    btn.Click += gridClicked(X, Y);
+                    btn.Content = "Shoot";
+                    buttonArray[X, Y] = btn;
+                    btn.Margin = new Thickness(2, 2, 2, 2);
+                    ViewGrid.Children.Add(btn);
+                    Grid.SetColumn(btn, X);
+                    Grid.SetRow(btn, Y);
+                }
+            }
+
+        }
+
+        public void Reveal()
+        {
             for (int i = 0; i < gridSize; i++)
             {
                 for (int j = 0; j < gridSize; j++)
                 {
-                    Button btn = new Button();
-                    btn.Click += gridClicked(i, j);
-                    btn.Content = "knap?";
-                    buttonArray[i, j] = btn;
-                    btn.Margin = new Thickness(2, 2, 2, 2);
-                    ViewGrid.Children.Add(btn);
-                    Grid.SetColumn(btn, i);
-                    Grid.SetRow(btn, j);
+                    buttonArray[i, j].Content = gridArray[i, j];
+                }
+            }
+        }
+        private void Clear()
+        {
+            dimensionX.Clear();
+            dimensionY.Clear();
+        }
+        private void BattleshipDirection(int antal)
+        {
+            randomNumberX = random.Next(0, gridSize);
+            randomNumberY = random.Next(0, gridSize);
+            int dimension = random.Next(1,3);
 
+            for (int i = 0; i < 10; i++)
+            {
+                dimensionX.Add(0);
+                dimensionY.Add(0);
+            }
 
+            if (dimension == 1)
+            {
+                dimensionX.Clear();
+                for (int i = 0; i < antal; i++)
+                {
+                    dimensionX.Add(i);
+                }
+            }
+            else if (dimension == 2)
+            {
+                dimensionY.Clear();
+                for (int i = 0; i < antal; i++)
+                {
+                    dimensionY.Add(i);
+                }
+            }
+        }
+        public void CreateBattleShip()
+        {
+            AddTwo();
+            AddThree();
+            AddFour();
+            AddFive();
+            FillGrid();
+        }
+        public void FillGrid()
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    if (gridArray[x, y] == null)
+                    {
+                        gridArray[x, y] = "X";
+                    }
+                }
+            }
+        }
+        public void Rules()
+        {
+            
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    if (gridArray[x,y] != "X" || gridArray[x,y] != "Shoot") ships++;
+                }
+            }
+            if (ships == 1) GameOver();
+            else ships = 0;
+        }
+        public void GameOver()
+        {
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    buttonArray[i, j].IsEnabled = false;
+                    
+                }
+            }
+            CounterTextBlock.Text = "Du har vundet! \nDu ramte forbi " + shotsFired + " gange.";
+        }
 
+        public void AddTwo()
+        {
+            Here: ;
+            Clear();
+            int minAntal = 2;
+            int maxAntal = gridSize - minAntal;
+            BattleshipDirection(minAntal);
+
+            if (randomNumberX > maxAntal || randomNumberY > maxAntal) goto Here;
+            else
+            {
+                if (gridArray[randomNumberX, randomNumberY] == null &&
+                    gridArray[randomNumberX + dimensionX[1], randomNumberY + dimensionY[1]] == null)
+                {
+                    gridArray[randomNumberX, randomNumberY] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[1], randomNumberY + dimensionY[1]] = "Ship" + minAntal;
+
+                }
+                else
+                {
+                    goto Here;
+                }
+            }
+        }
+        public void AddThree()
+        {
+            Here:;
+            Clear();
+            int minAntal = 3;
+            int maxAntal = gridSize - minAntal;
+            BattleshipDirection(minAntal);
+
+            if (randomNumberX > maxAntal || randomNumberY > maxAntal) goto Here;
+            else
+            {
+                if (gridArray[randomNumberX, randomNumberY] == null &&
+                    gridArray[randomNumberX + dimensionX[1], randomNumberY + dimensionY[1]] == null &&
+                    gridArray[randomNumberX + dimensionX[2], randomNumberY + dimensionY[2]] == null)
+                {
+                    gridArray[randomNumberX, randomNumberY] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[1], randomNumberY + dimensionY[1]] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[2], randomNumberY + dimensionY[2]] = "Ship" + minAntal;
+
+                }
+                else
+                {
+                    goto Here;
+                }
+            }
+        }
+        public void AddFour()
+        {
+            Here:;
+            Clear();
+            int minAntal = 4;
+            int maxAntal = gridSize - minAntal;
+            BattleshipDirection(minAntal);
+
+            if (randomNumberX > maxAntal || randomNumberY > maxAntal) goto Here;
+            else
+            {
+                if (gridArray[randomNumberX, randomNumberY] == null &&
+                    gridArray[randomNumberX + dimensionX[1], randomNumberY + dimensionY[1]] == null &&
+                    gridArray[randomNumberX + dimensionX[2], randomNumberY + dimensionY[2]] == null &&
+                    gridArray[randomNumberX + dimensionX[3], randomNumberY + dimensionY[3]] == null)
+                {
+                    gridArray[randomNumberX, randomNumberY] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[1], randomNumberY + dimensionY[1]] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[2], randomNumberY + dimensionY[2]] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[3], randomNumberY + dimensionY[3]] = "Ship" + minAntal;
+
+                }
+                else
+                {
+                    goto Here;
+                }
+            }
+        }
+        public void AddFive()
+        {
+            Here:;
+            Clear();
+            int minAntal = 5;
+            int maxAntal = gridSize - minAntal;
+            BattleshipDirection(minAntal);
+
+            if (randomNumberX > maxAntal || randomNumberY > maxAntal) goto Here;
+            else
+            {
+                if (gridArray[randomNumberX, randomNumberY] == null &&
+                    gridArray[randomNumberX + dimensionX[1], randomNumberY + dimensionY[1]] == null &&
+                    gridArray[randomNumberX + dimensionX[2], randomNumberY + dimensionY[2]] == null &&
+                    gridArray[randomNumberX + dimensionX[3], randomNumberY + dimensionY[3]] == null &&
+                    gridArray[randomNumberX + dimensionX[4], randomNumberY + dimensionY[4]] == null)
+                {
+                    gridArray[randomNumberX, randomNumberY] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[1], randomNumberY + dimensionY[1]] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[2], randomNumberY + dimensionY[2]] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[3], randomNumberY + dimensionY[3]] = "Ship" + minAntal;
+                    gridArray[randomNumberX + dimensionX[4], randomNumberY + dimensionY[4]] = "Ship" + minAntal;
+
+                }
+                else
+                {
+                    goto Here;
                 }
             }
         }
 
+
         private RoutedEventHandler gridClicked(int i, int j)
         {
-            return (btn, e) => buttonArray[i, j].Content = "ok";
+            return (btn, e) =>
+            {
+                buttonArray[i, j].Content = gridArray[i, j];
+                buttonArray[i, j].IsEnabled = false;
+                if (gridArray[i, j] == "X")
+                {
+                    shotsFired++;
+                    CounterTextBlock.Text = "Shots Missed: " + shotsFired;
+                }
+                Rules();
+            };
         }
-            }
+    }
 }
